a34c6ef37354ea73ca345e2fe9fa82340d9a4bdd
Juani Medone <juan.medone@lambdaclass.com>
2024-07-19 16:31:13 -0300

    Add documentation to main.rs and lib.rs

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,16 @@ const CELL_SIZE: f32 = 10.0;
 const HEIGHT: usize = 60;
 const WIDTH: usize = 80;
 
+/// The entry point of the Conway's Game of Life.
+///
+/// This function initializes the game with the specified height, width,
+/// and cell size. If there is an error in the parameters, it prints the error.
+///
+/// # Constants
+///
+/// * `CELL_SIZE` - The size of each cell.
+/// * `HEIGHT` - The height of the game grid.
+/// * `WIDTH` - The width of the game grid.
 #[macroquad::main("Conway's Game of Life")]
 async fn main() {
     if let Err(err) = game::start(HEIGHT, WIDTH, CELL_SIZE).await {
