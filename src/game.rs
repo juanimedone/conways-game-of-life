6bf4f205cacc55a72608104c817283414a07a7cf
Juani Medone <juan.medone@lambdaclass.com>
2024-07-19 15:54:45 -0300

    Add tests and refactor functions
    - Simplified get_index by removing unnecesary bound wrapping.
    - count_neighbors checks that neither x or y are out of bounds.
    - Extracted new function validate_start_parameters from start
    - Added tests for validate_start_parameters, (Game) new, get_index, count_neighbors and update

--- src/game.rs
+++ src/game.rs
@@ -13,20 +13,27 @@ impl Game {
     }
 
     fn get_index(&self, x: i32, y: i32) -> usize {
-        ((y as usize) % self.height) * self.width + ((x as usize) % self.width)
+        y as usize * self.width + x as usize
     }
 
     fn count_neighbors(&self, x: i32, y: i32) -> usize {
         let mut count = 0;
         for dx in -1..=1 {
+            let nx = x + dx;
+            if nx < 0 || nx >= self.width as i32 {      // checks if neighbor's x is out of bounds
+                continue;
+            }
             for dy in -1..=1 {
-                if dx != 0 || dy != 0 {
-                    let nx = x + dx;
-                    let ny = y + dy;
-                    let index = self.get_index(nx, ny);
-                    if self.cells[index] {
-                        count += 1;
-                    }
+                let ny = y + dy;
+                if ny < 0 || ny >= self.height as i32 {     // checks if neighbor's y is out of bounds
+                    continue;
+                }
+                if nx == x && ny == y { 
+                    continue;
+                }
+                let index = self.get_index(nx, ny);
+                if self.cells[index] {
+                    count += 1;
                 }
             }
         }
@@ -69,13 +76,18 @@ impl Game {
     }
 }
 
-pub async fn start(height: usize, width: usize, cell_size: f32) -> Result<(), String> {
+fn validate_start_parameters(height: usize, width: usize, cell_size: f32) -> Result<(), String> {
     if height == 0 || width == 0 {
-        return Err("Neither height or width can be 0".to_string());
+        return Err("Neither height nor width can be 0".to_string());
     }
     if cell_size <= 0.0 {
         return Err("Cell size can't be negative or 0".to_string());
     }
+    Ok(())
+}
+
+pub async fn start(height: usize, width: usize, cell_size: f32) -> Result<(), String> {
+    validate_start_parameters(height, width, cell_size)?;
 
     let mut game = Game::new(height, width);
     loop {
@@ -86,4 +98,87 @@ pub async fn start(height: usize, width: usize, cell_size: f32) -> Result<(), St
 
         next_frame().await
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test_valid_start_parameters() {
+        assert!(validate_start_parameters(60, 80, 10.0).is_ok())
+    }
+    
+    #[test]
+    fn test_invalid_start_parameters() {
+        match validate_start_parameters(0, 80, 10.0) {
+            Err(e) => assert_eq!(e, "Neither height nor width can be 0"),
+            _ => panic!("Expected an error"),
+        }
+        match validate_start_parameters(60, 0, 10.0) {
+            Err(e) => assert_eq!(e, "Neither height nor width can be 0"),
+            _ => panic!("Expected an error"),
+        }
+        match validate_start_parameters(60, 80, 0.0) {
+            Err(e) => assert_eq!(e, "Cell size can't be negative or 0"),
+            _ => panic!("Expected an error"),
+        }
+        match validate_start_parameters(60, 80, -1.0) {
+            Err(e) => assert_eq!(e, "Cell size can't be negative or 0"),
+            _ => panic!("Expected an error"),
+        }
+    }
+
+    #[test]
+    fn test_create_new_game() {
+        let game = Game::new(5, 6);
+        assert_eq!(game.cells.len(), 30)
+    }
+
+    #[test]
+    fn test_get_index() {
+        let game = Game::new(5, 5);
+        assert_eq!(game.get_index(0, 0), 0);
+        assert_eq!(game.get_index(4, 0), 4);
+        assert_eq!(game.get_index(0, 4), 20);
+        assert_eq!(game.get_index(4, 4), 24);
+    }
+    
+    #[test]
+    fn test_count_neighbors() {
+        let mut game = Game::new(5, 5);
+        game.cells = vec![
+            false, true,  false, true,  false,
+            true,  true,  true,  false, true,
+            false, false, true,  false, false,
+            true,  false, false, true,  true,
+            false, true,  false, true,  false,
+        ];
+        assert_eq!(game.count_neighbors(0, 0), 3);
+        assert_eq!(game.count_neighbors(0, 4), 2);
+        assert_eq!(game.count_neighbors(2, 2), 3);
+        assert_eq!(game.count_neighbors(4, 4), 3);
+    }
+
+    #[test]
+    fn test_update() {
+        let mut game = Game::new(5, 5);
+        game.cells = vec![
+            false, true,  false, true,  false,
+            true,  true,  true,  false, true,
+            false, false, true,  false, false,
+            true,  false, false, true,  true,
+            false, true,  false, true,  false,
+        ];
+        game.update();
+        let expected = vec![
+            true,  true,  false,  true,  false,
+            true,  false, false, false, false,
+            true,  false, true, false, true,
+            false, true, false, true, true,
+            false, false, true, true, true,
+        ];
+        assert_eq!(game.cells, expected);
+    }
+
 }
