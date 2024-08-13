3fadd1deb8e949becbaee494b7521a531ad4967c
Juani Medone <juan.medone@lambdaclass.com>
2024-08-13 16:06:38 -0300

    Move board out of game
    - Invert relationship: `board` executes main loop taking `game` as a parameter.
    - Refactor various functions.
    - Update tests.

--- src/game.rs
+++ src/game.rs
@@ -1,12 +1,8 @@
-use crate::board_renderer::BoardRenderer;
-use macroquad::prelude::*;
-
-const DEFAULT_SPEED: f32 = 10.0;
-
 /// Represents the state of the Game of Life.
 pub struct Game {
-    board: BoardRenderer,
-    cells: Vec<Vec<bool>>,
+    pub cells: Vec<Vec<bool>>,
+    ncols: usize,
+    nrows: usize,
 }
 
 impl Game {
@@ -14,102 +10,19 @@ impl Game {
     ///
     /// # Arguments
     ///
-    /// * `nrows` - The number of rows in the game grid.
+    /// * `board` - A `BoardRenderer` instance for rendering the game board.
     /// * `ncols` - The number of columns in the game grid.
-    /// * `cell_size` - The size of each cell in pixels.
+    /// * `nrows` - The number of rows in the game grid.
     ///
     /// # Returns
     ///
-    /// A new `Game` instance with the specified dimensions and cell size.
-    pub fn new(nrows: usize, ncols: usize, cell_size: usize) -> Self {
-        let board = BoardRenderer {
+    /// A new `Game` instance with the specified board renderer, dimensions, and with all cells initially dead.
+    pub fn new(ncols: usize, nrows: usize) -> Self {
+        let cells = vec![vec![false; nrows]; ncols];
+        Self {
+            cells,
             ncols,
             nrows,
-            cell_size,
-        };
-        let cells = vec![vec![false; nrows]; ncols];
-        Self { board, cells }
-    }
-
-    /// Runs the Game of Life.
-    ///
-    /// This function manages the game's lifecycle, including displaying the start menu,
-    /// allowing the player to choose the initial state of the cells, and continuously updating
-    /// and rendering the game state. The game can be paused or unpaused by pressing the Space
-    /// key, and the game speed can be adjusted using the Up and Down arrow keys.
-    ///
-    /// The `run` function performs the following actions in its main loop:
-    /// - Checks for key presses to pause/unpause the game, adjust the game speed or show the menu.
-    /// - Updates the game state if the game is not paused.
-    /// - Draws the game grid and cells.
-    /// - Displays the menu when requested and handles restarting the game if necessary.
-    /// - Waits for the next frame to be drawn, allowing for smooth animation.
-    ///
-    /// The game speed controls the frequency of state updates. The `speed` variable is
-    /// adjusted by multiplying or dividing it by 1.5 when the Up or Down arrow keys are pressed,
-    /// respectively. The update timer ensures the game state updates according to the current speed.
-    pub async fn run(&mut self) {
-        BoardRenderer::show_start_menu().await;
-        self.choose_initial_state().await;
-
-        let mut paused = false;
-        let mut show_menu = false;
-        let mut restart = false;
-        let mut speed = DEFAULT_SPEED;
-        let mut update_timer = 0.0;
-        loop {
-            self.check_keys(&mut paused, &mut speed, &mut show_menu);
-
-            if !paused {
-                update_timer += get_frame_time();
-                if update_timer >= 1.0 / speed {
-                    self.update();
-                    update_timer = 0.0;
-                }
-            }
-            if show_menu {
-                BoardRenderer::draw_menu(&mut paused, &mut restart).await;
-            }
-            if restart {
-                self.restart().await;
-                paused = false;
-                show_menu = false;
-                restart = false;
-            }
-
-            clear_background(BLACK);
-            self.board.draw_grid();
-            self.board.draw_cells(&self.cells);
-            next_frame().await;
-        }
-    }
-
-    /// Allows the player to choose the initial alive cells through a GUI.
-    ///
-    /// This function allows the player to click on cells to toggle their state
-    /// (alive or dead) before starting the game. The player can also randomize
-    /// the initial state by pressing 'R'.
-    async fn choose_initial_state(&mut self) {
-        let mut choosing = true;
-
-        while choosing {
-            clear_background(BLACK);
-            self.board.draw_grid();
-            self.board.draw_cells(&self.cells);
-
-            if is_mouse_button_pressed(MouseButton::Left) {
-                self.toggle_cell_state();
-            }
-            if is_key_pressed(KeyCode::R) {
-                self.randomize();
-            }
-
-            BoardRenderer::show_initial_instructions();
-            next_frame().await;
-
-            if is_key_pressed(KeyCode::Enter) {
-                choosing = false;
-            }
         }
     }
 
@@ -117,12 +30,8 @@ impl Game {
     ///
     /// This function retrieves the mouse position and toggles the state of the cell
     /// at that position from alive to dead or vice versa.
-    fn toggle_cell_state(&mut self) {
-        let mouse_pos = mouse_position();
-        let x = (mouse_pos.0 / self.board.cell_size as f32) as usize;
-        let y = (mouse_pos.1 / self.board.cell_size as f32) as usize;
-
-        if x < self.board.ncols && y < self.board.nrows {
+    pub fn toggle_cell_state(&mut self, x: usize, y: usize) {
+        if x < self.ncols && y < self.nrows {
             self.cells[x][y] = !self.cells[x][y];
         }
     }
@@ -130,44 +39,22 @@ impl Game {
     /// Randomizes the state of all cells in the grid.
     ///
     /// This function sets each cell in the grid to a random state (alive or dead).
-    fn randomize(&mut self) {
-        self.cells = (0..self.board.ncols)
-            .map(|_| (0..self.board.nrows).map(|_| ::rand::random()).collect())
+    pub fn randomize(&mut self) {
+        self.cells = (0..self.cells.len())
+            .map(|_| (0..self.cells[0].len()).map(|_| ::rand::random()).collect())
             .collect();
-    }
-
-    /// Checks for key presses to pause/unpause the game, adjust the speed and show the menu.
-    ///
-    /// # Arguments
-    ///
-    /// * `paused` - A mutable reference to a boolean that indicates whether the game is paused.
-    /// * `speed` - A mutable reference to a float representing the current game speed.
-    /// * `show_menu` - A mutable reference to a boolean that controls the visibility of the menu.
-    fn check_keys(&mut self, paused: &mut bool, speed: &mut f32, show_menu: &mut bool) {
-        if is_key_pressed(KeyCode::Space) {
-            *paused = !*paused;
-        }
-        if is_key_pressed(KeyCode::Up) {
-            *speed = (*speed * 1.5).min(1000.0);
-        }
-        if is_key_pressed(KeyCode::Down) {
-            *speed = (*speed / 1.5).max(0.1);
-        }
-        if is_key_pressed(KeyCode::M) {
-            *show_menu = !*show_menu;
-        }
     }
 
     /// Updates the game state to the next generation.
     ///
     /// This function calculates the next state of the game based on the current state
     /// and updates the cells accordingly.
-    fn update(&mut self) {
-        let mut next_cells = vec![vec![false; self.board.nrows]; self.board.ncols];
+    pub fn update(&mut self) {
+        let mut next_cells = vec![vec![false; self.nrows]; self.ncols];
 
         #[allow(clippy::needless_range_loop)] // this way is clearer than how Clippy suggests
-        for x in 0..self.board.ncols {
-            for y in 0..self.board.nrows {
+        for x in 0..self.ncols {
+            for y in 0..self.nrows {
                 let cell = self.cells[x][y];
                 let neighbors = self.count_neighbors(x as i32, y as i32);
 
@@ -191,13 +78,13 @@ impl Game {
         let mut count = 0;
         for dx in -1..=1 {
             let nx = x + dx;
-            if nx < 0 || nx >= self.board.ncols as i32 {
+            if nx < 0 || nx >= self.ncols as i32 {
                 // checks if neighbor's x is out of bounds
                 continue;
             }
             for dy in -1..=1 {
                 let ny = y + dy;
-                if ny < 0 || ny >= self.board.nrows as i32 {
+                if ny < 0 || ny >= self.nrows as i32 {
                     // checks if neighbor's y is out of bounds
                     continue;
                 }
@@ -211,15 +98,6 @@ impl Game {
         }
         count
     }
-
-    /// Resets the game state and restarts the game.
-    ///
-    /// This function clears the current state of the cells, effectively resetting the game board
-    /// to its initial empty state. It then prompts the user to choose a new initial state for the cells.
-    async fn restart(&mut self) {
-        self.cells = vec![vec![false; self.board.nrows]; self.board.ncols];
-        self.choose_initial_state().await;
-    }
 }
 
 #[cfg(test)]
@@ -228,7 +106,7 @@ mod tests {
 
     #[test]
     fn test_create_new_game() {
-        let game = Game::new(30, 40, 10);
+        let game = Game::new(40, 30);
         assert_eq!(game.cells.len(), 40);
         assert_eq!(game.cells[0].len(), 30);
     }
@@ -236,7 +114,7 @@ mod tests {
     #[rustfmt::skip]
     #[test]
     fn test_count_neighbors() {
-        let mut game = Game::new(5, 5, 1);
+        let mut game = Game::new(5, 5);
         game.cells = vec![
             vec![false, true, false, true, false],
             vec![true, true, true, false, true],
@@ -253,7 +131,7 @@ mod tests {
     #[rustfmt::skip]
     #[test]
     fn test_update() {
-        let mut game = Game::new(5, 5, 1);
+        let mut game = Game::new(5, 5);
         game.cells = vec![
             vec![false, true, false, true, false],
             vec![true, true, true, false, true],
